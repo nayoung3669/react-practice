e46e8c0daba47abb7fa0916cdd16216fb9bd548a
nayoung3669 <skdud3669@gmail.com>
2023-07-04 14:32:54 +0900

    모달 init

---
+++ modal-prac/src/components/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <h1>Button</h1>
+
+      <button>button</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ modal-prac/src/components/Input.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <h1>Input</h1>
+      <input />
+    </div>
+  );
+};
+
+export default Input;
